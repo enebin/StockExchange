ab785b61c0112cfb46e157b45cfbf9739f1bf0e0
enebin <46271447+enebin@users.noreply.github.com>
2020-08-13 23:14:06 +0900

    m

--- Crawl/DATA_Crawl_Lite.py
+++ Crawl/DATA_Crawl_Lite.py
@@ -3,14 +3,12 @@ import os.path
 import time
 import bcolors
 import pandas as pd
-import pandas_datareader.data as web
 import multiprocessing as mp
 import logging
 
 from datetime import datetime
 from tqdm import tqdm
 from urllib.request import urlopen
-from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 
 
@@ -28,11 +26,9 @@ def chunks(input_list, size):
         yield input_list[element: element + size]
 
 
-# 종목 등락률을 계산합니다.
-def get_FR(cur, prev):
-    fr = ((cur - prev) / prev) * 100
-    fr = round(fr, 2)
-    return fr
+def initialize(input_list):
+    for x in input_list:
+        x = 0
 
 
 # 종목 코드 리스트를 가져온 후 전처리합니다. init 함수에서 클래스 생성과 함께 실행됩니다.
@@ -48,123 +44,16 @@ def get_code_list(market, m_type, in_path):
 
 
 # 쓰레딩을 위해 사용하는 스타트 함수입니다.
-def starter(input_code, glob, period):
-    if period == 'week':
-        name, value_tag = get_data(input_code)
+def starter(input_code, glob):
+    name, curPrice, prevPrice, value_tag = get_all(input_code)
 
-        # 에러상황(ETF, 리츠 등 펀드류 코드 경우)시 리턴합니다.
-        if name == -1:
-            logging.warning(input_code)
-            return
-        else:
-            temp_row = make_data_frame(name, input_code, value_tag)
-            glob.df = glob.df.append(temp_row)
-
-    elif period == 'day':
-        curPrice, prevPrice = get_price(input_code)
-        temp_row = make_price_frame(input_code, curPrice, prevPrice)
-        glob.df = glob.df.append(temp_row)
-
-    elif period == 'all':
-        name, curPrice, prevPrice, value_tag = get_all(input_code)
-
-        # 에러상황(ETF, 리츠 등 펀드류 코드 경우)시 리턴합니다.
-        if name == -1:
-            logging.warning(input_code)
-            return
-        else:
-            temp_row = make_all_frame(name, input_code, curPrice, prevPrice, value_tag)
-            glob.df = glob.df.append(temp_row)
-
-
-# 종목코드 하나를 받아 투자지표를 크롤링합니다. [종목명, [투자지표]]를 반환합니다.
-
-
-def get_data(input_code):
-    # 종목코드를 가져와 NAVER 증권 정보 URL에 대입합니다.
-    url = "https://finance.naver.com/item/main.nhn?code=" + input_code
-
-    # BS4를 이용한 HTML 소스 크롤링입니다.
-    url_result = urlopen(url)
-    html = url_result.read()
-    soup = BeautifulSoup(html, 'lxml')
-
-    # 잘못된 코드나 ETF를 솎아내기 위해 예외처리를 하였습니다.
-    try:
-        # HTML 소스에서 회사명을 찾아 저장합니다.
-        name = soup.find("div", {"class": "wrap_company"}).find("h2").text
-
-        # HTML 소스에서 투자지표 Table을 찾아 저장한 후 값을 추출합니다.
-        table_tag = soup.find("table", {"class": "per_table"})
-        values_raw = table_tag.find_all("em")
-
-        return name, values_raw
-
-    except AttributeError:
-        print(bcolors.ERRMSG + "ERROR OCCURS\n" + bcolors.ITALIC +
-              "Possible Error: It can be ETF, REITs, etc...")
+    # 에러상황(ETF, 리츠 등 펀드류 코드 경우)시 리턴합니다.
+    if name == -1:
         logging.warning(input_code)
-        return -1, [-1]
-
-
-# 종목코드 하나를 받아 주가를 받아옵니다. [현재가, 전일종가]를 반환합니다.
-def get_price(input_code, try_cnt=1):
-    """
-    # 야후 파이낸스릉 이용한 방법. 없는 데이터가 너무 많다.
-    if market == 'KOSPI':
-        code_mod = code + '.KS'
-    elif market == 'TEST':
-        code_mod = code + '.KS'
+        return
     else:
-        code_mod = code + '.KQ'
-
-    today = datetime.today().strftime('%Y-%m-%d')
-
-    try:
-        prices = web.DataReader(code_mod, "yahoo", today)
-        print("url opened")
-        curPrice = prices.Close.iloc[0]
-        prevPrice = prices.Open.iloc[0]
-    except KeyError:
-        print(code_mod)
-    """
-
-    """
-    # KRX 한국 거래소를 이용한 방법. 빠를땐 광속이나 대부분 더럽게 느리다. 
-    url = "http://asp1.krx.co.kr/servlet/krx.asp.XMLSiseEng?code={}".format(code)
-    req = urlopen(url)
-    print("url opened")
-    result = req.read()
-    xmlsoup = BeautifulSoup(result, "lxml-xml")
-    curPrice = xmlsoup.find("TBL_StockInfo").attrs["CurJuka"]
-    prevPrice = xmlsoup.find("TBL_StockInfo").attrs["PrevJuka"]
-
-    curPrice = remove_coma(curPrice)
-    prevPrice = remove_coma(prevPrice)
-    """
-
-    # 네이버 금융을 이용한 방법. 그나마 가장 안정적이고 준수하다.
-    # 소스에서 현재가를 찾아 저장합니다. (주로 크롤링한 날 종가)
-    url = "https://finance.naver.com/item/main.nhn?code=" + input_code
-
-    # BS4를 이용한 HTML 소스 크롤링입니다.
-    url_result = urlopen(url)
-    html = url_result.read()
-    soup = BeautifulSoup(html, 'lxml')
-
-    try:
-        # 소스에서 현재가를 찾아 저장합니다. (주로 크롤링한 날 종가)
-        curPrice = soup.find("p", {"class": "no_today"}).text
-        curPrice = remove_coma(curPrice.split('\n')[2])
-
-        prevPrice = soup.find("td", {"class": "first"}).text
-        prevPrice = remove_coma(prevPrice.split('\n')[3])
-
-    except AttributeError:
-        print(bcolors.ERRMSG + "ERROR OCCURS\n" + bcolors.ITALIC +
-              "Possible Error: It can be transaction suspension, delisting, etc...")
-
-    return curPrice, prevPrice
+        temp_row = make_all_frame(name, input_code, curPrice, prevPrice, value_tag)
+        glob.df = glob.df.append(temp_row)
 
 
 # 주가와 지표 모두 받아옵니다.
@@ -202,49 +91,6 @@ def get_all(input_code):
         return -1, -1, -1, [-1]
 
 
-# get_data 에서 크롤링한 데이터를 받아 데이터프레임에 저장하는 함수입니다.
-# 데이터프레임을 반환합니다.
-def make_data_frame(name, input_code, values_raw):
-    # 크롤링으로 받은 HTML 소스(values_raw)에서 PER, PBR 등의 값을 추출합니다.
-    values = []
-    for i in values_raw:
-        content = i.text
-        if content == 'N/A':
-            values.append('N/A')
-        else:
-            values.append(remove_coma(content))
-
-    # 1줄짜리 임시 데이터프레임을 구성하여 데이터를 저장합니다.
-    temp_data = pd.DataFrame(columns=("NAME", "CODE", 'CUR PRICE', 'PREV PRICE', 'FR',
-                                      "PER", "EPS", "E_PER", "E_EPS", "PBR", "BPS", "ITR"))
-
-    temp_data.loc[0, 'NAME'] = name
-    temp_data.loc[0, 'CODE'] = input_code
-    temp_data.loc[0, 'PER':"ITR"] = values
-
-    # 진행상황을 체크하며 값을 확인합니다.
-    # print(temp_data.tail(1))
-
-    # 데이터프레임을 반환합니다.
-    return temp_data
-
-
-# 종목코드를 받아 주식의 현재가, 전일 종가, 등락률을 저장하는 함수입니다.
-# 데이터프레임을 반환합니다.
-def make_price_frame(input_code, curPrice, prevPrice):
-    temp_data = pd.DataFrame(columns=['CODE', 'CUR PRICE', 'PREV PRICE', 'FR'])
-
-    temp_data.loc[0, 'CODE'] = input_code
-    temp_data.loc[0, 'CUR PRICE'] = curPrice
-    temp_data.loc[0, 'PREV PRICE'] = prevPrice
-    temp_data.loc[0, 'FR'] = float(get_FR(curPrice, prevPrice))
-
-    # 진행상황을 체크하며 값을 확인합니다.
-    # print(res.head())
-
-    return temp_data
-
-
 def make_all_frame(name, input_code, curPrice, prevPrice, values_raw):
     # 크롤링으로 받은 HTML 소스(values_raw)에서 PER, PBR 등의 값을 추출합니다.
     values = []
@@ -273,7 +119,7 @@ def make_all_frame(name, input_code, curPrice, prevPrice, values_raw):
 
 
 # 프로그램이 끝났다면 CSV 파일로 저장한 후 종료합니다.
-def merger(globs, code_list, period, in_path, out_path):
+def merger(globs, code_list, in_path, out_path):
     result = measurements
     for glob in globs:
         result = result.append(glob.df)
@@ -293,16 +139,9 @@ def merger(globs, code_list, period, in_path, out_path):
 
     if os.path.isfile(file_name):
         object_df = pd.read_csv(file_name)
-
-        if period == 'day':
-            result.iloc[:, 10:16] = object_df.iloc[:, 10:16]
-        elif period == 'week':
-            result.loc[:, 'CUR PRICE'] = object_df.loc[:, 'CUR PRICE']
-            result.loc[:, 'PREV PRICE'] = object_df.loc[:, 'PREV PRICE']
-        elif period == 'all':
-            result.loc[:, 'PER':"ITR"] = object_df.loc[:, 'PER':"ITR"]
-            result.loc[:, 'CUR PRICE'] = object_df.loc[:, 'CUR PRICE']
-            result.loc[:, 'PREV PRICE'] = object_df.loc[:, 'PREV PRICE']
+        result.loc[:, 'PER':"ITR"] = object_df.loc[:, 'PER':"ITR"]
+        result.loc[:, 'CUR PRICE'] = object_df.loc[:, 'CUR PRICE']
+        result.loc[:, 'PREV PRICE'] = object_df.loc[:, 'PREV PRICE']
     else:
         pass
 
@@ -315,18 +154,13 @@ def merger(globs, code_list, period, in_path, out_path):
     print('\n')
 
 
-def multiprocess(globs, period, code_list, in_path, out_path, numberOfThreads=8):
+def multiprocess(globs, code_list, in_path, out_path, numberOfThreads=8):
     # 값들을 저장할 Pandas 데이터프레임을 구성합니다.
     # Multiprocessing 을 위한 전처리도 같이 합니다.
     # 프로세스의 개수 (MAX = 18)
     if numberOfThreads > 18:
         print(bcolors.ERRMSG + "Too much process. Check number of threads again")
         return
-
-    if period == 'week':
-        print(bcolors.OKMSG + "Processing type is: Investment index.")
-    else:
-        print(bcolors.OKMSG + "Processing type is: Price informations.")
 
     for glob in globs:
         glob.df = measurements
@@ -335,7 +169,7 @@ def multiprocess(globs, period, code_list, in_path, out_path, numberOfThreads=8)
     processes = []
     index = 0
     for code in tqdm(code_list, desc="Processing DATA"):
-        process = mp.Process(target=starter, args=(str(code), globs[index], str(period)))
+        process = mp.Process(target=starter, args=(str(code), globs[index]))
         index = (index + 1) % numberOfThreads
         processes.append(process)
 
@@ -359,12 +193,7 @@ def multiprocess(globs, period, code_list, in_path, out_path, numberOfThreads=8)
         # for glob in globs:
         #     print(glob.df.tail(1), end='\n')
 
-    merger(globs, code_list, period, in_path, out_path)
-
-
-def initialize(input_list):
-    for x in input_list:
-        x = 0
+    merger(globs, code_list, in_path, out_path)
 
 
 # ========아래로 메인 코드입니다========= #
@@ -385,7 +214,6 @@ if __name__ == '__main__':
     for a in unit_test:
         market = a[0]
         m_type = a[1]
-        period = a[2]
 
         in_path = './DATA/' + market + '_' + m_type + '.csv'
         out_path = './DATA/Results/DATA_' + market + '_' + m_type + datetime.today().strftime("_%Y%m%d") + '.csv'
@@ -422,7 +250,7 @@ if __name__ == '__main__':
 
         # 멀티 프로세스는 속도 향상을 위해 필요합니다. n개의 프로세스를 사용해 속도를 n배로 끌어 올립니다.
         # 기본값은 8입니다.
-        multiprocess(globs, period=period, code_list=code_list, in_path=in_path, out_path=out_path, numberOfThreads=6)
+        multiprocess(globs, code_list=code_list, in_path=in_path, out_path=out_path, numberOfThreads=6)
 
         initialize(globs)
         initialize(code_list)
