9ca4b37fea842ee4b919418b56aafd34fa7622c7
enebin <46271447+enebin@users.noreply.github.com>
2020-08-22 23:22:44 +0900

    m

--- prac.py
+++ prac.py
@@ -1,9 +1,18 @@
-from pykiwoom.kiwoom import *
+import pandas as pd
+import requests
 
-kiwoom = Kiwoom()
-kiwoom.CommConnect(block=True)
+URL = "https://finance.naver.com/item/main.nhn?code=005930"
 
-group = kiwoom.GetThemeGroupList(1)
-print(group)
-tickers = kiwoom.GetThemeGroupCode('141')
-print(tickers)
+samsung_electronic = requests.get(URL)
+html = samsung_electronic.text
+
+financial_stmt = pd.read_html(samsung_electronic.text)[3]
+
+financial_stmt.set_index(('주요재무정보', '주요재무정보', '주요재무정보'), inplace=True)
+financial_stmt.index.rename('주요재무정보', inplace=True)
+financial_stmt.columns = financial_stmt.columns.droplevel(2)
+financial_stmt = financial_stmt.iloc[1, :]
+print(type(financial_stmt))
+financial_stmt.to_csv("./prac.csv", encoding='utf-8-sig')
+
+
